26beca88f0ede20e44e866b4d44e1cf243f1c08d
Petr Shevtsov <petr.shevtsov@gmail.com>
2015-05-28 21:38:09 +0600

    Parser redone

--- parser.go
+++ parser.go
@@ -1,11 +1,52 @@
-package main
+package elblog
 
 import (
+	"fmt"
+
 	"github.com/pshevtsov/gonx"
 )
 
-const (
-	parserFormat = `$timestamp $elb $client $backend $request_processing_time $backend_processing_time $response_processing_time $elb_status_code $backend_status_code $received_bytes $sent_bytes "$request"`
+var (
+	FieldTimestamp              = "timestamp"
+	FieldELB                    = "elb"
+	FieldClient                 = "client"
+	FieldBackend                = "backend"
+	FieldRequestProcessingTime  = "request_processing_time"
+	FieldBackendProcessingTime  = "backend_processing_time"
+	FieldResponseProcessingTime = "response_processing_time"
+	FieldELBStatus              = "elb_status_code"
+	FieldBackendStatus          = "backend_status_code"
+	FieldReceivedBytes          = "received_bytes"
+	FieldSentBytes              = "sent_bytes"
+	FieldRequest                = "request"
 )
 
-var Parser = gonx.NewParser(parserFormat)
+type field string
+
+func (f field) String() string {
+	s := string(f)
+	if s == FieldRequest {
+		return fmt.Sprintf(`"$%s"`, s)
+	}
+	return fmt.Sprint("$", s)
+}
+
+func NewParser() *gonx.Parser {
+	slice := []field{
+		field(FieldTimestamp),
+		field(FieldELB),
+		field(FieldClient),
+		field(FieldBackend),
+		field(FieldRequestProcessingTime),
+		field(FieldBackendProcessingTime),
+		field(FieldResponseProcessingTime),
+		field(FieldELBStatus),
+		field(FieldBackendStatus),
+		field(FieldReceivedBytes),
+		field(FieldSentBytes),
+		field(FieldRequest),
+	}
+	format := fmt.Sprint(slice)
+	format = format[1 : len(format)-1]
+	return gonx.NewParser(format)
+}
